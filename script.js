e738e659af0271f49d35ca85d5d3a9c3d0e6f8a4
RamonRocha06 <rocharamon227@gmail.com>
2024-11-05 20:58:36 -0300

    primeiro-commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const formu=document.querySelector(".form")
+const masca=document.querySelector(".mascara-form")
+
+function clica(){
+    formu.style.left="50%"
+    formu.style.transform="translateX(-50%)"
+    masca.style.visibility="visible"
+}
+
+
+const formular=document.querySelector(".form")
+const mascara=document.querySelector(".mascara-form")
+
+function cliquei (){
+    formular.style.left="-400px"
+    mascara.style.visibility="hidden"
+
+}
+
+
